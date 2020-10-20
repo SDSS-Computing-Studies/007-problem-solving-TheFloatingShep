0402dcb221cd1640de9fc5e6355351cdd5598afc
Kenji MacMartin <47537573+TheFloatingShep@users.noreply.github.com>
2020-10-20 12:29:32 -0700

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -50,35 +50,35 @@ def buy():
 
     # All the stuff
     # Second line
-    if p.pixelMatchesColor(1037,675, (115,105,96)):
+    if p.pixelMatchesColor(1037,675, (141,126,114)):
         p.moveTo(1037,675)
         p.click(1037,296)
     if p.pixelMatchesColor(1100,710, (224,136,74)):
         p.moveTo(1100,700)
         p.click(1100,700)
 
-    if p.pixelMatchesColor(1037,582, (115,105,96)):
+    if p.pixelMatchesColor(1037,582, (141,126,114)):
         p.moveTo(1037,582)
         p.click(1037,582)
     if p.pixelMatchesColor(1100,610, (224,136,74)):
         p.moveTo(1100,610)
         p.click(1100,610)
 
-    if p.pixelMatchesColor(1037,487, (115,105,96)):
+    if p.pixelMatchesColor(1037,487, (141,126,114)):
         p.moveTo(1037,487)
         p.click(1037,487)
     if p.pixelMatchesColor(1100,510, (224,136,74)):
         p.moveTo(1100,510)
         p.click(1100,510)
 
-    if p.pixelMatchesColor(1037,393, (115,105,96)):
+    if p.pixelMatchesColor(1037,393, (141,126,114)):
         p.moveTo(1037,393)
         p.click(1037,393)
     if p.pixelMatchesColor(1100,410, (224,136,74)):
         p.moveTo(1100,410)
         p.click(1100,410)
 
-    if p.pixelMatchesColor(1037,300, (115,105,96)):
+    if p.pixelMatchesColor(1037,300, (141,126,114)):
         p.moveTo(1037,300)
         p.click(1037,300)
     if p.pixelMatchesColor(1100,330, (224,136,74)):
@@ -87,35 +87,35 @@ def buy():
 
 
     # First line
-    if p.pixelMatchesColor(664,675, (115,105,96)):
+    if p.pixelMatchesColor(664,675, (141,126,114)):
         p.moveTo(610,675)
         p.click(610,296)
     if p.pixelMatchesColor(730,710, (224,136,74)):
         p.moveTo(730,710)
         p.click(730,710)
 
-    if p.pixelMatchesColor(664,582, (115,105,96)):
+    if p.pixelMatchesColor(664,582, (141,126,114)):
         p.moveTo(610,582)
         p.click(610,582)
-    if p.pixelMatchesColor(730,610, (224,136,74)):
+    if p.pixelMatchesColor(730,610, (141,126,114)):
         p.moveTo(730,610)
         p.click(730,610)
 
-    if p.pixelMatchesColor(664,487, (115,105,96)):
+    if p.pixelMatchesColor(664,487, (141,126,114)):
         p.moveTo(610,487)
         p.click(610,487)
     if p.pixelMatchesColor(730,510, (224,136,74)):
         p.moveTo(730,510)
         p.click(730,510)
 
-    if p.pixelMatchesColor(664,393, (115,105,96)):
+    if p.pixelMatchesColor(664,393, (141,126,114)):
         p.moveTo(610,393)
         p.click(610,393)
     if p.pixelMatchesColor(730,410, (224,136,74)):
         p.moveTo(730,410)
         p.click(730,410)
 
-    if p.pixelMatchesColor(664,300, (115,105,96)):
+    if p.pixelMatchesColor(664,300, (141,126,114)):
         p.moveTo(610,300)
         p.click(610,300)
     if p.pixelMatchesColor(730,330, (224,136,74)):

351c55f8d5cb5d46011c04d9aff7523282535ea5
Kenji MacMartin <47537573+TheFloatingShep@users.noreply.github.com>
2020-10-20 12:23:57 -0700

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -5,6 +5,7 @@ import pyautogui as p
 
 p.mouseInfo()
 
+# Buys upgrades
 def upgrades():
     p.moveTo(347,450)
     p.click(347,450)
@@ -13,12 +14,32 @@ def upgrades():
         if p.pixelMatchesColor(1060,590, (225,137,74)):
             p.moveTo(1060,590)
             p.click(1060,590)
+            p.moveTo(1256,235)
+            time.sleep(0.5)
+        else:
+            p.click(1256,235)
+            break
 
+# Buys managers
 def managers():
     p.moveTo(347,520)
     p.click(347,520)
+    time.sleep(0.5)
+    while True:
+        if p.pixelMatchesColor(1048,492, (151,191,212)):
+            p.moveTo(1048,492)
+            p.click(1048,492)
+            p.moveTo(1256,235)
+            time.sleep(0.5)
+            while p.pixelMatchesColor(930,380, (176,219,126)):
+                p.moveTo(930,380)
+                p.click(930,380)
+        else:
+            p.click(1256,235)
+            break
 
-def run():
+# Buys stuff
+def buy():
     # Upgrades
     if p.pixelMatchesColor(347,450, (220,123,54)):
         upgrades()
@@ -27,55 +48,93 @@ def run():
     if p.pixelMatchesColor(347,520, (220,123,54)):
         managers()
 
-    p.moveTo(980,675)
+    # All the stuff
+    # Second line
+    if p.pixelMatchesColor(1037,675, (115,105,96)):
+        p.moveTo(1037,675)
+        p.click(1037,296)
     if p.pixelMatchesColor(1100,710, (224,136,74)):
         p.moveTo(1100,700)
         p.click(1100,700)
 
-    p.moveTo(980,580)
+    if p.pixelMatchesColor(1037,582, (115,105,96)):
+        p.moveTo(1037,582)
+        p.click(1037,582)
     if p.pixelMatchesColor(1100,610, (224,136,74)):
         p.moveTo(1100,610)
         p.click(1100,610)
 
-    p.moveTo(980,490)
+    if p.pixelMatchesColor(1037,487, (115,105,96)):
+        p.moveTo(1037,487)
+        p.click(1037,487)
     if p.pixelMatchesColor(1100,510, (224,136,74)):
         p.moveTo(1100,510)
         p.click(1100,510)
 
-    p.moveTo(980,410)
+    if p.pixelMatchesColor(1037,393, (115,105,96)):
+        p.moveTo(1037,393)
+        p.click(1037,393)
     if p.pixelMatchesColor(1100,410, (224,136,74)):
         p.moveTo(1100,410)
         p.click(1100,410)
 
-    p.moveTo(980,296)
+    if p.pixelMatchesColor(1037,300, (115,105,96)):
+        p.moveTo(1037,300)
+        p.click(1037,300)
     if p.pixelMatchesColor(1100,330, (224,136,74)):
         p.moveTo(1100,330)
         p.click(1100,330)
 
 
-    p.moveTo(610,675)
+    # First line
+    if p.pixelMatchesColor(664,675, (115,105,96)):
+        p.moveTo(610,675)
+        p.click(610,296)
     if p.pixelMatchesColor(730,710, (224,136,74)):
         p.moveTo(730,710)
         p.click(730,710)
 
-    p.moveTo(610,580)
+    if p.pixelMatchesColor(664,582, (115,105,96)):
+        p.moveTo(610,582)
+        p.click(610,582)
     if p.pixelMatchesColor(730,610, (224,136,74)):
         p.moveTo(730,610)
         p.click(730,610)
 
-    p.moveTo(610,490)
+    if p.pixelMatchesColor(664,487, (115,105,96)):
+        p.moveTo(610,487)
+        p.click(610,487)
     if p.pixelMatchesColor(730,510, (224,136,74)):
         p.moveTo(730,510)
         p.click(730,510)
 
-    p.moveTo(610,410)
+    if p.pixelMatchesColor(664,393, (115,105,96)):
+        p.moveTo(610,393)
+        p.click(610,393)
     if p.pixelMatchesColor(730,410, (224,136,74)):
         p.moveTo(730,410)
         p.click(730,410)
 
-    p.moveTo(610,296)
+    if p.pixelMatchesColor(664,300, (115,105,96)):
+        p.moveTo(610,300)
+        p.click(610,300)
     if p.pixelMatchesColor(730,330, (224,136,74)):
         p.moveTo(730,330)
         p.click(730,330)
 
-run()
+# Loops program
+def run():
+    while True:
+        buy()
+        time.sleep(15)
+
+# Changes buy to MAX and then runs
+def start():
+    if p.pixelMatchesColor(1249,198, (224,139,78)):
+        p.moveTo(1249,198)
+        p.click(1249,198)
+        time.sleep(0.4)
+        start()
+    else:
+        run()
+start()
